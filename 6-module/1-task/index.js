3c4f15b20ade1f882e79bd7d288299c8859db85b
Vladislav <ma1rosssss@gmail.com>
2024-05-30 20:16:18 +0300

    решение задачки №1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,43 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.edit();
+  }
+
+  edit() {
+    const table = document.createElement("table");
+    table.innerHTML = `
+    <tr>
+    <th>Имя</th>
+    <th>Возраст</th>
+    <th>Зарплата</th>
+    <th>Город</th>
+    <th></th>
+  </tr>`;
+
+    this.rows.forEach((element) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+      <td>${element.name}</td>
+      <td>${element.age}</td>
+      <td>${element.salary}</td>
+      <td>${element.city}</td>
+      <td><button>X</button></td>`;
+      table.appendChild(row);
+    });
+
+    this.btnOff(table);
+
+    return table;
+  }
+
+  btnOff(table) {
+    const btn = table.querySelectorAll("button");
+    btn.forEach((button) => {
+      button.addEventListener("click", () => {
+        button.parentElement.parentElement.remove();
+      });
+    });
   }
 }
