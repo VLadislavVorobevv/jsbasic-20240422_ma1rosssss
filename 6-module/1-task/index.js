ac071c06406423db91e060131f664a0aede48954
Vladislav <ma1rosssss@gmail.com>
2024-08-20 18:45:14 +0300

    решение 6 модуля

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,56 +1,50 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
-    this.rows = rows;
-    this.elem = this.edit();
+    this.elem = document.createElement("table");
+
+    this.elem.innerHTML = `
+      <thead>
+          <tr>
+            <td>Имя</td>
+            <td>Возраст</td>
+            <td>Зарплата</td>
+            <td>Город</td>
+            <td></td>
+          </tr>
+      </thead>
+    `;
+
+    let tableInner = rows
+      .map((row) => {
+        let cellsWithData = Object.values(row) // для каждого значения из объекта row
+          .map((value) => `<td>${value}</td>`) // обернуть его в <td>
+          .join(""); // полученный массив <td>...</td> объединить в одну строку
+
+        return `
+          <tr>
+            ${cellsWithData}
+            <td><button>X</button></td>
+          </tr>
+        `; // возвращаем верстку одной строки
+      })
+      .join("");
+
+    this.elem.innerHTML += `
+      <tbody>
+        ${tableInner}
+      <tbody>
+    `; // оборачиваем полученные строчки в tbody
+
+    this.elem.addEventListener("click", (event) => this.onClick(event));
   }
 
-  edit() {
-    const table = document.createElement("table");
-    table.innerHTML = `
-    <tr>
-    <th>Имя</th>
-    <th>Возраст</th>
-    <th>Зарплата</th>
-    <th>Город</th>
-    <th></th>
-  </tr>`;
+  onClick(event) {
+    if (event.target.tagName != "BUTTON") {
+      return;
+    }
 
-    this.rows.forEach((element) => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-      <td>${element.name}</td>
-      <td>${element.age}</td>
-      <td>${element.salary}</td>
-      <td>${element.city}</td>
-      <td><button>X</button></td>`;
-      table.appendChild(row);
-    });
+    let tr = event.target.closest("tr");
 
-    this.btnOff(table);
-
-    return table;
-  }
-
-  btnOff(table) {
-    const btn = table.querySelectorAll("button");
-    btn.forEach((button) => {
-      button.addEventListener("click", () => {
-        button.parentElement.parentElement.remove();
-      });
-    });
+    tr.remove();
   }
 }
